1f52b2435c77daf6b3fb0e2edfcf4711b6b99c7f
igor <igorpigidin@gmail.com>
2023-09-21 15:25:16 +0700

    работа с assign

--- arrays/objects2.js
+++ arrays/objects2.js
@@ -11,11 +11,11 @@ admin.name = 'Pete'; // изменено по ссылке из переменн
 
 // alert(user.name); // 'Pete', изменения видны по ссылке из переменной "user"
 
-let obj1 = {
+let obj21 = {
     "sv1": 42
 }
 
-let obj2 = {
+let obj22 = {
     "sv2": 33
 }
 
@@ -23,6 +23,25 @@ let res = {
     "baza": 52
 }
 
-Object.assign(res, obj1, obj2)
+Object.assign(res, obj21, obj22)
 
-console.log(res)
+// console.log(res)
+
+let tiger = {
+    fuck: "pizda"
+}
+
+let obj23 = {
+    animalFamily: {
+        cat: tiger,
+        monkey: "orangutan",
+    },
+    peoples: {
+        friend: "vitalik",
+        emeny: "Egor",
+    }
+}
+
+let clone = Object.assign({}, res, obj23)
+
+console.log(clone)
