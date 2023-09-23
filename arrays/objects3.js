2c678d4cb14c525cf6b0819b316c8067042e3200
igor <igorpigidin@gmail.com>
2023-09-24 02:37:39 +0700

    работа с созданием объектов

--- arrays/objects3.js
+++ arrays/objects3.js
@@ -1,6 +1,13 @@
 let me = {
     name: "Igor",
     game: "dota 2",
+    age: 23,
+    hobbies: ["программирование", "игры", "музыка"],
+    address: {
+        city: "Киев",
+        street: "Крещатик",
+        house: 1
+    },
 
     play() {
         console.log(this.name + ' любит играть в ' + this.game)
@@ -8,8 +15,24 @@ let me = {
 
     sayHi: function() {
         console.log(this.name + " говорит привет")
+    },
+
+    // Новые методы
+    introduce() {
+        console.log(`Привет! Меня зовут ${this.name}, мне ${this.age} лет. Я люблю ${this.game}.`);
+    },
+
+    addHobby(hobby) {
+        this.hobbies.push(hobby);
+        console.log(`Добавлено новое хобби: ${hobby}`);
+    },
+
+    getHobbies() {
+        return this.hobbies.join(", ");
+    },
+
+    updateAge(newAge) {
+        this.age = newAge;
+        console.log(`Возраст обновлен на ${newAge}`);
     }
 }
-
-me.play()
-
