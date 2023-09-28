fc49f2e311079465fd28a1764f244f0a44328df9
igor <igorpigidin@gmail.com>
2023-09-28 20:06:37 +0700

    работа с string

---
+++ arrays/srtings.js
@@ -0,0 +1,19 @@
+let str11 = 'привет меня зовут Игорь'
+
+let name11 = str11.indexOf("Игорь")
+
+// console.log(name11)
+
+let isIncludeName = str11.includes("Игорь")
+
+// console.log(isIncludeName)
+
+let startWithName = str11.startsWith("Игорь")
+let endWithName = str11.endsWith("Игорь")
+
+// console.log(startWithName) /false
+// console.log(endWithName) /true
+
+let sliceStr = str11.slice(name11)
+
+// console.log(sliceStr)
