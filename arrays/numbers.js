ce3aabf88cac7e85207144b2ca28e63a98989fe2
igor <igorpigidin@gmail.com>
2023-09-28 18:39:33 +0700

    работа с number

---
+++ arrays/numbers.js
@@ -0,0 +1,23 @@
+let number1 = 1000000
+let number2 = 1_000_000
+
+// console.log(number1 === number2) true
+
+let number3 = 1e6
+
+// console.log(number1 === number3) true
+
+// console.log(number1.toString(2)) двочиное
+
+let numInStr = '123px'
+
+// console.log(parseInt(numInStr))
+
+let randomNum = Math.random()*10
+
+// console.log(randomNum.toFixed(1))
+
+let numArr = [1, 2, 1, 10, 33, 12, 23, 11]
+
+// console.log(Math.max(...numArr))
+// console.log(Math.min(...numArr))
