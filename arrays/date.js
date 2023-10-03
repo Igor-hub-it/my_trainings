419831fd193f8a02ad943c23f4f64bd93cfa23fb
igor <igorpigidin@gmail.com>
2023-10-03 17:54:12 +0700

    работа с date

--- arrays/date.js
+++ arrays/date.js
@@ -9,14 +9,18 @@ let date2 = new Date(2025, 11, 10, 15, 12)
 let now1 = Date.now()
 let today = new Date(now1)
 
-console.log(today.getFullYear())
-console.log(today.getMonth())
+// console.log(today.getFullYear())
+// console.log(today.getMonth())
 
-console.log(today.getDate())
+// console.log(today.getDate())
 
-console.log(today.getHours())
-console.log(today.getMinutes())
-console.log(today.getSeconds())
-console.log(today.getMilliseconds())
-console.log(today.getDay())
+// console.log(today.getHours())
+// console.log(today.getMinutes())
+// console.log(today.getSeconds())
+// console.log(today.getMilliseconds())
+// console.log(today.getDay())
 
+let now2 = new Date().getTime()
+// это быстрее чем Date.now()
+
+// console.log(now2)
