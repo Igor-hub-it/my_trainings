dc6df237a90b87b3db1c074857263eef8f9317b2
igor <igorpigidin@gmail.com>
2023-10-07 22:31:10 +0700

    rest & json

--- arrays/rest.js
+++ arrays/rest.js
@@ -13,5 +13,5 @@ func(1, 2, 3, 4, 5, 5, 4, 4)
 
 let arrRest = [1, 2, 3, 4, 5, 5, 4, 4]
 
-console.log(Math.max(...arrRest))
-console.log(arrRest.indexOf(Math.max(...arrRest)))
+// console.log(Math.max(...arrRest))
+// console.log(arrRest.indexOf(Math.max(...arrRest)))
