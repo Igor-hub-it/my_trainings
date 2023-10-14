7e6bf47a0181fd44588e808e6ead034926fcf356
igor <igorpigidin@gmail.com>
2023-10-15 00:11:24 +0700

    promise

--- arrays/promise.js
+++ arrays/promise.js
@@ -17,3 +17,33 @@ let promise2 = new Promise((resolve, reject) => {
         })
 })
 
+let data = promise2
+    .then(result => {
+        // console.log('успех, братья', result)
+    })
+    .catch(result => {
+        // console.error('ошибка', result)
+    })
+
+// async yo = function() {
+//     await console.log(data)
+// }
+
+function delay(ms) {
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, ms)
+    })
+}
+  
+// delay(3000).then(() => alert('выполнилось через 3 секунды'));
+
+let showCircle = () => {
+    return new Promise((resolve, reject) => {
+        resolve
+    })
+}
+
+// showCircle(150, 150, 100).then(div => {
+//     div.classList.add('message-ball');
+//     div.append("Hello, world!");
+// });
