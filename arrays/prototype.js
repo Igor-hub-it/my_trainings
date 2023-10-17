1b7f29afb05b144314bb9713342ad9345c3c26b6
igor <igorpigidin@gmail.com>
2023-10-17 11:16:17 +0700

    proto

--- arrays/prototype.js
+++ arrays/prototype.js
@@ -22,3 +22,24 @@ rabbit.__proto__ = animal
 // console.log(rabbit.life, rabbit.eats, rabbit.jumps)
 
 // console.log(Object.keys(rabbit))
+
+let objPrototype = function(value) {
+    return this.name = value
+}
+
+let userPrototype = new objPrototype("Igor")
+
+let userPrototype2 = new userPrototype.constructor('Roman')
+
+// console.log(userPrototype)
+// console.log(userPrototype2)
+
+Function.prototype.delay = function(time) {
+    setTimeout(this, time)
+}
+
+function fooo() {
+    console.log("хуи")
+}
+
+// fooo.delay(1000)
