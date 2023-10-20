2ff8c2da719fe045d6771264fb13eb3de100edca
igor <igorpigidin@gmail.com>
2023-10-20 13:48:09 +0700

    class

--- arrays/class.js
+++ arrays/class.js
@@ -15,4 +15,31 @@ class MyClass {
 
 let myFuck1 = new MyClass("Igor", "men")
 
-console.log(myFuck1)
+// console.log(myFuck1)
+
+class Animal {
+    constructor(name, speed) {
+        this.name = name
+        this.speed = speed
+        this.position = 0
+    }
+
+    run() {
+        this.position += speed
+    }
+
+    rename(name) {
+        this.name = name
+    }
+}
+
+class Cat extends Animal {
+    constructor(name, speed, color) {
+        super(name, speed)
+        this.color = color
+    }
+}
+
+let Lych = new Cat("Lych", 999, 'white')
+
+// console.log(Lych)
